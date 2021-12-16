fbb8fcc23071ee25f9381d04cf196726803b9278
Thiago Santos <33657279+thiago-hds@users.noreply.github.com>
2021-12-16 20:49:29 -0300

    questao 3: api calculo

---
+++ gdfs-workspace/www/api/calculo.php
@@ -0,0 +1,51 @@
+<?php 
+
+require_once '../bootstrap.php';
+
+try{
+    $categoriaId = $_POST['categoria_id'];
+    $cidadeId = $_POST['cidade_id'];
+    $enderecoPartida = $_POST['endereco_partida'];
+    $enderecoDestino = $_POST['endereco_destino'];
+
+    $distancia = rand(0, 100); 
+    $duracao = round(rand(0, 60) / 60, 2); 
+    $hora = new DateTime('now', new DateTimeZone('America/Sao_Paulo'));
+
+    $categoria = $repository->getCategoria($categoriaId);
+
+    $total = $categoria['bandeirada'] 
+        + $distancia * $categoria['valor_km']
+        + $duracao *  $categoria['valor_hora'];
+    $total = round($total, 2);
+
+    $dadosHistorico = [
+        'cidade_id' => $cidadeId,
+        'categoria_id' => $categoriaId,
+        'endereco_partida' => $enderecoPartida,
+        'endereco_destino' => $enderecoDestino,
+        'bandeirada' => $categoria['bandeirada'],
+        'valor_hora' => $categoria['valor_hora'],
+        'valor_km' => $categoria['valor_km'],
+        'total' => $total,
+        'hora' => $hora->format('Y-m-d H:i:s'),
+        'distancia' => $distancia,
+        'duracao' => $duracao,
+    ];
+
+    $repository->saveHistorico($dadosHistorico);
+
+    $resposta = $dadosHistorico;
+}
+catch(Exception $ex){
+    $resposta = [
+        'status' => 'erro',
+        'message' => $ex->getMessage()
+    ];
+}
+
+header('Content-Type', 'application/json');
+echo json_encode($dadosHistorico);
+
+
+
--- gdfs-workspace/www/repository.php
+++ gdfs-workspace/www/repository.php
@@ -26,4 +26,29 @@ class Repository
 
         return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function getCategoria($id)
+    {
+        $query = "SELECT * FROM categorias WHERE id = ?;";
+
+        $statement = $this->db->prepare($query);
+        $statement->execute([$id]);
+
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function saveHistorico($data)
+    {
+        $query = "
+            INSERT INTO historicos(cidade_id, categoria_id, endereco_partida, endereco_destino, bandeirada, valor_hora, valor_km, total, hora, distancia, duracao)
+            VALUES (:cidade_id, :categoria_id, :endereco_partida, :endereco_destino, :bandeirada, :valor_hora, :valor_km, :total, :hora, :distancia, :duracao)
+        ";
+
+        $statement = $this->db->prepare($query);
+        $statement->execute($data);
+
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    
 }
